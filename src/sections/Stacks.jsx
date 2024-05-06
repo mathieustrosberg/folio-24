04fc426a38597ff7676aa0cdea85ce466b89b6b1
mathieustrosberg <mathieustrosberg@gmail.com>
2024-05-06 15:47:56 +0200

    update title stacks

--- src/sections/Stacks.jsx
+++ src/sections/Stacks.jsx
@@ -5,7 +5,7 @@ import StackList from "@/composants/StackList";
 const Stacks = () => {
   return (
     <div className="flex md:flex-row flex-col gap-10">
-      <h2 className="flex flex-col h-100 w-5/12 gap-10 text-sm">
+      <h2 className="flex flex-col h-100 md:w-5/12 gap-10 text-sm">
         Compétences en développement
       </h2>
       <div className="flex flex-col md:w-8/12 leading-7 gap-6 text-sm">
